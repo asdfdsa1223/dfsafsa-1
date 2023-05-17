574ec71f17d2756dc3f79d7451e5b280fc8c2cf5
asdfdsa1223 <91037929+asdfdsa1223@users.noreply.github.com>
2023-05-17 21:37:15 +0800

    Update jd_speed_sign.js

--- src/jd_speed_sign.js
+++ src/jd_speed_sign.js
@@ -29,10 +29,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 
-let maxThread = 5; //并发数
-if ($.isNode() && process.env.JDSPEED_MAXTHREAD){
-	maxThread=process.env.JDSPEED_MAXTHREAD*1;
-}
+
 
 let TaskList = [];
 let llAPIError = false;
